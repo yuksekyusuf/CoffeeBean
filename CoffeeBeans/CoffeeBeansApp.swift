8351601fa6e160e4ef729c06f8c8b0566304e302
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2021-12-23 22:01:44 -0500

    Initial Commit

---
+++ CoffeeBeans/CoffeeBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBeansApp.swift
+//  CoffeeBeans
+//
+//  Created by Ahmet Yusuf Yuksek on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
